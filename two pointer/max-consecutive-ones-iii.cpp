8fcbd05b680e88f89f38d2ec402981c41e913005
Your Name <18695602710@163.com>
2021-02-23 23:16:47 +0800

    2021-2-23 23:16:45

--- two pointer/max-consecutive-ones-iii.cpp
+++ two pointer/max-consecutive-ones-iii.cpp
@@ -102,3 +102,58 @@ class Solution {
         return ans;
     }
 };
+
+/* 类似题目：
+今天，书店老板有一家店打算试营业 customers.length 分钟。每分钟都有一些顾客（customers[i]）会进入书店，所有这些顾客都会在那一分钟结束后离开。
+
+在某些时候，书店老板会生气。 如果书店老板在第 i 分钟生气，那么 grumpy[i] =
+1，否则 grumpy[i] = 0。
+当书店老板生气时，那一分钟的顾客就会不满意，不生气则他们是满意的。
+
+书店老板知道一个秘密技巧，能抑制自己的情绪，可以让自己连续 X
+分钟不生气，但却只能使用一次。
+
+请你返回这一天营业下来，最多有多少客户能够感到满意的数量。
+ 
+
+示例：
+
+输入：customers = [1,0,1,2,1,1,7,5], grumpy = [0,1,0,1,0,1,0,1], X = 3
+输出：16
+解释：
+书店老板在最后 3 分钟保持冷静。
+感到满意的最大客户数量 = 1 + 1 + 1 + 1 + 7 + 5 = 16.
+ 
+
+来源：力扣（LeetCode）
+链接：https://leetcode-cn.com/problems/grumpy-bookstore-owner
+著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。 */
+
+class Solution {
+   public:
+    int maxSatisfied(vector<int>& customers, vector<int>& grumpy, int X) {
+        // 损失客户数量前缀和
+        // 算出损失数量最大的X段
+        int left = 0, tmpMax = 0;
+        int n = grumpy.size();
+        int lSum = 0, rSum = 0;
+        for (int right = 0; right < n; ++right) {
+            while (right - left + 1 > X) {
+                lSum += customers[left] * grumpy[left];
+                left++;
+            }
+            rSum += customers[right] * grumpy[right];
+            tmpMax = max(tmpMax, rSum - lSum);
+        }
+
+        int res = 0;
+        for (int i = 0; i < n; ++i) {
+            if (grumpy[i] == 0) {
+                res += customers[i];
+            }
+        }
+
+        res += tmpMax;
+        return res;
+    }
+};
